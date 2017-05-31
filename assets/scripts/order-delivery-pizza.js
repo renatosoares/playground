05285ccfe69a0d54a9122822245c16f57b9b9c5f
Renato Soares <renatosoarespro@hotmail.com>
2017-05-31 09:47:30 -0300

    Added asynchronous delivery requests

---
+++ assets/scripts/order-delivery-pizza.js
@@ -0,0 +1,63 @@
+function getCustomerInfo() {
+    var phone = document.getElementById("phone").value;
+    var url = "app/modules/order-delivery/order-delivery-lookupCustomer.php?phone=" +
+        escape(phone);
+    request.open("GET", url, true);
+    request.onreadystatechange = updatePage;
+    request.send(null);
+}
+
+function updatePage() {
+    if (request.readyState == 4) {
+        if (request.status == 200) {
+            /* Get the response from the server */
+            var customerAddress = request.responseText;
+
+            /* Update the HTML web form */
+            document.getElementById("address").value =
+                customerAddress;
+        } else
+            alert("Error! Request status is " + request.status);
+    }
+}
+
+function submitOrder() {
+    var phone = document.getElementById("phone").value;
+    var address = document.getElementById("address").value;
+    var order = document.getElementById("order").value;
+    var url = "app/modules/order-delivery/order-delivery-placeOrder.php";
+    request.open("POST", url, true);
+    request.onreadystatechange = showConfirmation;
+    request.setRequestHeader("Content-Type",
+        "application/x-www-form-urlencoded");
+    request.send("phone=" + escape(phone) +
+        "&address=" + escape(address) +
+        "&order=" + escape(order));
+}
+
+function showConfirmation() {
+    if (request.readyState == 4) {
+        if (request.status == 200) {
+            var response = request.responseText;
+            // Locate form on page
+            var mainDiv = document.getElementById("main-page");
+            var orderForm = document.getElementById("order-form");
+
+            // Create some confirmation text
+            pElement = document.createElement("p");
+            textNode = document.createTextNode("Your order should arrive within " +
+                response + " minutes. Enjoy your pizza!");
+            pElement.appendChild(textNode);
+
+            // Replace the form with the confirmation
+            mainDiv.replaceChild(pElement, orderForm);
+        } else {
+            var message = request.getResponseHeader("Status");
+            if ((message == null) || (message.length <= 0)) {
+                alert("Error! Request status is " + request.status);
+            } else {
+                alert(message);
+            }
+        }
+    }
+}
